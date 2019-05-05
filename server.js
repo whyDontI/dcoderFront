4e55f4f0b51508dee7a229c274cd9f80eda0b827
whyDontI <nikhilbhandakar1998@gmail.com>
2019-05-05 14:20:45 +0530

    add new model response

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static('./dist/dcoderChallenge'));
+app.use(express.static('./dist/dcoder-challenge'));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, './dist/dcoderChallenge/index.html'));
+    res.sendFile(path.join(__dirname, './dist/dcoder-challenge/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
